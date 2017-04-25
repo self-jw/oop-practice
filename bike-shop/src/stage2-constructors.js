10e0f2dfcbc6e5da4c33d6ba72d4e81a32151a0a
Jolly Walia <jwalia@hammer.local>
2017-04-25 13:33:59 -0700

    Finshed stage2.

--- bike-shop/src/stage2-constructors.js
+++ bike-shop/src/stage2-constructors.js
@@ -1,14 +1,30 @@
-function Bike() {
-  // your code here
+function Bike(name, price) {
+  this.name = name;
+  this.price = price;
+
+  this.tires = [new Tire(), new Tire()];
+  this.frame = new Frame();
+  this.rings = [3, 7];
+  this.brakes = {
+    front: true,
+    back: true,
+  };
 }
 
-function Frame() {
-  // your code here
+function Frame(color, size, style) {
+  this.color = (typeof color !== 'undefined') ? color : 'black';
+  this.size = (typeof size !== 'undefined') ? size : 55;
+  this.style = (typeof style !== 'undefined') ? style : 'street';
 }
 
-function Tire() {
-  // your code here
+function Tire(diameter, type) {
+  this.diameter = (typeof diameter !== 'undefined') ? diameter : 22;
+  this.type = (typeof type !== 'undefined') ? type : 'street';
 }
+
+const myFrame = new Frame();
+const myTire = new Tire();
+const myBike = new Bike();
 
 module.exports = {
   Bike: Bike,
